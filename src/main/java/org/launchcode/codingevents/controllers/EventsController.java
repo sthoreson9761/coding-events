3e13f7e0b24e962eb1a8f77bd66a5117de61a9e7
Scott Thoreson <scott.thoreson@live.com>
2022-02-15 12:58:51 -0500

    Completed basic chapter 11 exercises

--- src/main/java/org/launchcode/codingevents/controllers/EventsController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventsController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Chris Bay
@@ -16,13 +17,24 @@ import java.util.ArrayList;
 @RequestMapping(value = "events")
 public class EventsController {
 
-    private static ArrayList<String> events = new ArrayList<>();
+//    private static ArrayList<String> events = new ArrayList<>();
+    private static HashMap<String,String> events = new HashMap<>();
 
+    public static void setEvents(HashMap<String, String> events) {
+        EventsController.events = events;
+    }
+
+    public static HashMap<String, String> getEvents() {
+        events.put("ECGC", "East Coast Gaming Conference");
+        events.put("GameJam", "Groups of participants are given 49 hours to create a game based on a theme");
+        events.put("CodeJam","Put your coding skills to the test as you work your way through multiple rounds of algorithmic coding puzzles for the title of Code Jam Champ and $15,000 USD.");
+        return events;
+    }
     // Lives at /events
     @GetMapping("")
     public String displayAllEvents(Model model) {
 
-        model.addAttribute("events", events);
+        model.addAttribute("events", getEvents());
         model.addAttribute("title", "All Events");
 
         return "events/index";
@@ -37,8 +49,8 @@ public class EventsController {
 
     // lives at /events/create
     @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String event) {
-        events.add(event);
+    public String processCreateEventForm(@RequestParam String event, @RequestParam String description) {
+        events.put(event,description);
         return "redirect:";
     }
 
